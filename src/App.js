dde41eefeb9fc59aa02e69055b821cdeb637f252
Fernandamaralss <fernanda.amaralss@outook.com>
2025-07-31 11:11:06 -0300

    Utilizando loop no header

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
+// Importa arquivos, componentes e imagens
 import './App.css';
 import Logo from './componentes/Logo'
+import perfil from './images/perfil.svg'
+import sacola from './images/sacola.svg'
+
+// Array com os textos e imagens dos icones das opções do menu principal
+const textoOpcoes = ["CATEGORIAS", "FAVORITOS", "MINHA ESTANTE"]
+const iconesOpcoes = [perfil, sacola]
 
 function App() {
   return (
@@ -7,9 +14,16 @@ function App() {
       <header className='App-header'>
         <Logo></Logo>
         <ul className='opcoes'>
-          <li className='opcao'><p>Categorias</p></li>
-          <li className='opcao'><p>Minha Estante</p></li>
-          <li className='opcao'><p>Favoritos</p></li>
+          { textoOpcoes.map( (texto) => ( // .map: percorrer um array e transformar seus itens, retornando um novo array com o resultado de cada transformação.
+            <li className='opcao'><p>{texto}</p></li>
+          )
+        )}
+        </ul>
+        <ul className='icones'>
+          { iconesOpcoes.map( (icone) => (
+            <li className='icone'><img src={icone}></img></li>
+          )
+        )}
         </ul>
       </header>
     </div>
